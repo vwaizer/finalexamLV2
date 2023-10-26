f6892493e159f7e6a32b284e7d36c609f4ff9b63
vwaizer <lightwing2208@gmail.com>
2023-10-26 10:03:47 +0700

    init

---
+++ final_exam/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Content from './component/Content';
+
+function App() {
+  return (
+    <div className="App">
+      <Content/>
+    </div>
+  );
+}
+
+export default App;
