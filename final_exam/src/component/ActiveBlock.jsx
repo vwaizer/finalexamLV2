9e8bab12a1008dffbfa91592b71099fe0e0f95c4
vwaizer <lightwing2208@gmail.com>
2023-10-27 22:52:47 +0700

    formatting code

--- final_exam/src/component/ActiveBlock.jsx
+++ final_exam/src/component/ActiveBlock.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import AllBlock from './AllBlock';
+import React from "react";
+import AllBlock from "./AllBlock";
 
 const ActiveBlock = (props) => {
-    const inputData=props.inputVar;
-    const setInputData=props.inputMethod;
-     const checkedList=props.checkVar;
-     const setCheckedList=props.checkMethod;
-     const active=true;
+  const inputData = props.inputVar;
+  const setInputData = props.inputMethod;
+  const checkedList = props.checkVar;
+  const setCheckedList = props.checkMethod;
+  const active = true;
   return (
-    <AllBlock inputVar={inputData} inputMethod={setInputData}  checkVar={checkedList} checkMethod={setCheckedList} isActive={active} />
-  )
-}
+    <AllBlock
+      inputVar={inputData}
+      inputMethod={setInputData}
+      checkVar={checkedList}
+      checkMethod={setCheckedList}
+      isActive={active}
+    />
+  );
+};
 
-export default ActiveBlock
+export default ActiveBlock;
